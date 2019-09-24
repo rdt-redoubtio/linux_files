72278be1b90144d207e651a8a52be8e3fb840dea
Robert D. Thurston <rdt@alasconnect.com>
2019-09-24 08:03:38 -0800

    Update head.c

--- src/head/head.c
+++ src/head/head.c
@@ -6,30 +6,46 @@
 
 int main(int argc, char *argv[])
 {
-    int fileDescriptor;
-    fileDescriptor = 0;
     if(argc != 2)
     {
         printf("\n Usage: \n");
+        return 1;
     }
     else
     {
-        printf(argv[1]);
-        fileDescriptor = open(argv[1],O_RDONLY);
-
-        if(fileDescriptor == -1)
-        {
-            printf("\n Could not open file.\n");
-        }
-        else
-        {
-            printf("File Opened\n\n");
-            char buffer[400];
-            ssize_t bytes_read;
-            int bytes_to_read;
-            bytes_to_read = sizeof(buffer);
-            bytes_read = read(fileDescriptor, buffer, bytes_to_read);
-            printf(&buffer[0]);
-        }
+        int fileDescriptor = openFile(argc, argv);
+        char buffer[400];
+        int readStatus = readFile(fileDescriptor, buffer);
+        printf("\n");
+        printf(buffer);
+        printf("\n");
+        return 0;
     }
+    
+}
+
+int openFile(int argc, char *argv[])
+{
+    int fileDescriptor;
+    fileDescriptor = 0;
+    
+    fileDescriptor = open(argv[1],O_RDONLY);
+
+    if(fileDescriptor == -1)
+    {
+        return fileDescriptor;
+    }
+    else
+    {
+        return fileDescriptor;
+    }
+}
+
+int readFile(int fileDescriptor, char *buffer[])
+{
+    ssize_t bytesRead;
+    int bytesToRead = sizeof(buffer);
+    bytesRead = read(fileDescriptor, buffer, bytesToRead);
+    // TODO: Error handling
+    return 0;
 }
